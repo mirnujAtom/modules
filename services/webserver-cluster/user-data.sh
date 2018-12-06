96c9981add26b5830a462b4fb19dd36c08f7b982
Andrii Cherkasov <acherkas@cisco.com>
2018-12-06 15:42:32 +0200

    Zato1

--- services/webserver-cluster/user-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat > index.html <<EOF
-<h1>ZatoIchi is the best samurai movie</h1>
-<img src="https://thedullwoodexperiment.files.wordpress.com/2015/12/zotr-scene3.jpg">
-EOF
-
-nohup busybox httpd -f -p "${server_port}" &
