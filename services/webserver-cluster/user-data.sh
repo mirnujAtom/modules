ee90295eac6b58210750ca97d7579f8f4d16055d
Andrii Cherkasov <acherkas@cisco.com>
2018-12-06 15:25:27 +0200

    Zato1

--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cat > index.html <<EOF
-<h1>Hello, World</h1>
-
+<h1>ZatoIchi is the best samurai movie</h1>
+<img src="https://thedullwoodexperiment.files.wordpress.com/2015/12/zotr-scene3.jpg">
 EOF
 
 nohup busybox httpd -f -p "${server_port}" &
