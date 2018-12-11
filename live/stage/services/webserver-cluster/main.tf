acf3d14c635f423986d8b62cb3ed209228e06f31
Andrii Cherkasov <acherkas@cisco.com>
2018-12-11 14:54:36 +0200

    merged

---
+++ live/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "webserver_cluster" {
+  source = "git@github.com:mirnujAtom/modules.git//services/webserver-cluster?ref=webserver-cluster-1.0.1"
+
+  cluster_name           = "webservers-stage"
+
+  instance_type = "t2.micro"
+  min_size      = 1
+  max_size      = 2
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name  = "scale-out-during-business-hours"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 10
+  recurrence             = "0 9 * * *"
+  autoscaling_group_name = "${module.webserver_cluster.asg_name}"
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name  = "scale-in-at-night"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 2
+  recurrence             = "0 17 * * *"
+  autoscaling_group_name = "${module.webserver_cluster.asg_name}"
+}
