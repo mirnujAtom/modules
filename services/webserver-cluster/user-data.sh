59e893b0370d0ebc64d34e47e7f4009f79027d62
Andrii Cherkasov <acherkas@cisco.com>
2019-01-11 14:48:14 +0200

    width

--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -2,7 +2,7 @@
 
 cat > index.html <<EOF
 <h1>ZatoIchi still the best!</h1>
-<img src="https://reelgingermoviefan.files.wordpress.com/2013/11/zatoichi-2003-takeshi-kitano-original.jpg", width="800">
+<img src="https://reelgingermoviefan.files.wordpress.com/2013/11/zatoichi-2003-takeshi-kitano-original.jpg", width="1200">
 EOF
 
 nohup busybox httpd -f -p "${server_port}" &
